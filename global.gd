3b3a96111aadb13fc4028e4ae5d2cbd1d348b3b6
PlantvZombie <wintonajones@gmail.com>
2024-11-01 23:06:50 -0400

    Items

--- global.gd
+++ global.gd
@@ -1,1 +1,16 @@
 extends Node
+
+var itemname:Array
+var itemlevel:Array
+var Item:Array
+
+func _ready() -> void:
+	itemname = ["Baton", "Taser"]
+	for i in itemname:
+		itemlevel[i] = 0
+	itemlevel[0] = 1
+	itemlevel[1]= 1
+
+func _process(_delta: float) -> void:
+	for i in itemname:
+		Item[i] = itemname[i] + str(itemlevel[i])
