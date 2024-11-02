bea15a240b0830f4fe246142af90c826d2e7fd7b
PlantvZombie <wintonajones@gmail.com>
2024-11-02 11:12:29 -0400

    Fixes

--- global.gd
+++ global.gd
@@ -7,23 +7,21 @@ var Item:Array
 signal LevelUp
 var leveled:bool = false
 var maxplayerhp = 100
-var playerhealth = 70
+var playerhealth = 100
 var neededexp = 1000
-var exp = 145
+var exp = 0
 
 func _ready() -> void:
-	var maxplayerhp = 100
-	var playerhealth = 70
-	var neededexp = 1000
-	var exp = 145
-  connect("LevelUp", Level)
-	itemname = ["Baton", "Taser", "Drones", "Simulated Arm", "Firewall"]>>>>>>> main
+	connect("LevelUp", Level)
+	itemname = ["Baton", "Taser", "Drones", "Simulated Arm", "Firewall", "Drank"]
 	for i in itemname.size():
 		itemlevel.append(0)
 	itemlevel[0] = 1
 	itemlevel[1]= 1
 	for i in itemname.size():
 		Item.append(0)
+	for i in itemname.size():
+		Item[i] = itemname[i] + str(itemlevel[i])
 
 func _process(_delta: float) -> void:
 	if leveled:
