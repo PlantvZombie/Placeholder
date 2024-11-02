98e02711623cf24a6b7c5a8a304607a4373b89ee
NoahGBaker <palifonix@gmail.com>
2024-11-02 11:28:11 -0700

    NO DEPENDENCIES!!!!

--- soldier.gd
+++ soldier.gd
@@ -1,1 +1,15 @@
 extends CharacterBody2D
+
+@onready var player = $"../Player"
+var player_position
+var target_position
+var SPEED = 145.0
+
+func _physics_process(delta: float) -> void:
+	player_position = player.position
+	target_position = (player_position - position).normalized()
+	if target_position or player_position:
+		if position.distance_to(player_position) >= 5:
+			move_and_collide(player_position)
+	else:
+			pass
--- world.gd
+++ world.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("esc") and get_node("Inventory").is_visible() == false:
-		get_node("Inventory").set_visible(true)
+	if Input.is_action_just_pressed("esc") and get_node("Inventory2").is_visible() == false:
+		get_node("Inventory2").set_visible(true)
 	elif Input.is_action_just_pressed("esc"):
-		get_node("Inventory").set_visible(false)
-	get_node("Inventory").position = get_node("Player").position
+		get_node("Inventory2").set_visible(false)
+	get_node("Inventory2").position = get_node("Player").position
