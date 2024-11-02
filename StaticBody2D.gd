58e0df09d5bda2b255bf4e77bd579bb53ab4c2e0
PlantvZombie <wintonajones@gmail.com>
2024-11-01 20:00:08 -0400

    Movement Done

---
+++ StaticBody2D.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta):
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	var direction2 = Input.get_axis("up", "down")
+	if direction or direction2:
+		velocity.x = direction * SPEED
+		velocity.y = direction2 * SPEED
+		if sqrt(pow(velocity.x, 2) + pow(velocity.y, 2)) > 300: 
+			velocity.x = direction * SPEED * (sqrt(2)/2)
+			velocity.y = direction2 * SPEED * (sqrt(2)/2)
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
