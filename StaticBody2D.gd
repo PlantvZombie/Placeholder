d288b77d0555087938f6d0c50ab47138237bab1f
Crabion78 <149846477+Crabion78@users.noreply.github.com>
2024-11-02 17:43:07 -0400

    wa

--- StaticBody2D.gd
+++ StaticBody2D.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-var right
-
+var right:bool
+var atk:bool
+@onready var anim = $AnimatedSprite2D
 func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
@@ -16,6 +17,25 @@ func _physics_process(delta):
 		if sqrt(pow(velocity.x, 2) + pow(velocity.y, 2)) > 300: 
 			velocity.x = direction * SPEED * (sqrt(2)/2)
 			velocity.y = direction2 * SPEED * (sqrt(2)/2)
+		if Input.is_action_pressed("left"):
+			anim.play("Lwalk")
+			right = false
+		elif Input.is_action_pressed("right"):
+			anim.play("Rwalk")
+			right = true
+		elif right:
+			anim.play("Rwalk")
+		else:
+			anim.play("Lwalk")
+			
 	else:
 		velocity = Vector2.ZERO
+		if right:
+			anim.play("Ridle")
+		else:
+			anim.play("Lidle")
+	if atk and right:
+		anim.play("Ratk")
+	elif atk:
+		anim.play("Latk")
 	move_and_slide()
