4a5d8537d18df926a7802501d29af1258107fae1
PlantvZombie <wintonajones@gmail.com>
2024-11-02 15:50:01 -0400

    Move Speed + Attack Count Implemented

---
+++ level_up_screen.gd
@@ -0,0 +1,45 @@
+extends Control
+
+@onready var Items:Array = get_node("/root/Global").itemname
+var rng = RandomNumberGenerator.new()
+var opt:Array = [0, 0, 0]
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Global.levelup:
+		Leveled()
+		Global.levelup = false
+	if Global.leveled:
+		self.set_visible(false)
+
+func Leveled():
+	for i in opt.size():
+		rng.randomize()
+		opt[i] = Items[rng.randi_range(1, Items.size()-1)]
+		Items.remove_at(Items.find(opt[i]))
+	get_node("Panel/PanelGap/Components/Options/Option1").set_text(opt[0])
+	get_node("Panel/PanelGap/Components/Options/Option2").set_text(opt[1])
+	get_node("Panel/PanelGap/Components/Options/Option3").set_text(opt[2])
+	self.set_visible(true)
+
+
+func _on_option_1_pressed() -> void:
+	Global.itemlevel[Global.itemname.find(opt[0])] += 1
+	Global.leveled = true
+
+
+func _on_option_2_pressed() -> void:
+	Global.itemlevel[Global.itemname.find(opt[1])] += 1
+	Global.leveled = true
+
+
+func _on_option_3_pressed() -> void:
+	Global.itemlevel[Global.itemname.find(opt[2])] += 1
+	Global.leveled = true
